5d67684173ba186895636dd5a69d937972c93b64
Jelle Schutter <jelle@schutter.xyz>
2021-08-24 02:25:42 +0200

    passthrough original filename

--- src/handler.ts
+++ src/handler.ts
@@ -5,9 +5,12 @@ export async function handleRequest(request: Request): Promise<Response> {
     return new Response('Invalid URL. See: https://github.com/jelleschutter/tentative-ical#-how-to', { status: 400 });
   }
   const iCalendar = await getICalendar(iCalUrl);
+  const iCalPath = new URL(iCalUrl).pathname;
+  const iCalFilename = iCalPath.substring(iCalPath.lastIndexOf('/') + 1);
   return new Response(`${iCalendar}`, {
     headers: {
-      'Content-Type': 'text/calendar'
+      'Content-Type': 'text/calendar',
+      'Content-Disposition': `attachment; filename="${iCalFilename}"`
     }
   });
 }
