fae8e418395a52809d9abd89a682dea834b17171
Jelle Schutter <jelle@schutter.xyz>
2021-08-24 19:57:34 +0000

    improve error handling

--- src/handler.ts
+++ src/handler.ts
@@ -1,10 +1,13 @@
 export async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url);
   const iCalUrl = decodeURIComponent(url.pathname).substring(6);
-  if (!iCalUrl.startsWith('https://')) {
+  if (!(iCalUrl.startsWith('https://') || iCalUrl.startsWith('http://'))) {
     return new Response('Invalid URL. See: https://github.com/jelleschutter/tentative-ical#-how-to', { status: 400 });
   }
   const iCalendar = await getICalendar(iCalUrl);
+  if (iCalendar === '') {
+    return new Response('Could not detect a iCalendar.', { status: 400 });
+  }
   const iCalPath = new URL(iCalUrl).pathname;
   const iCalFilename = iCalPath.substring(iCalPath.lastIndexOf('/') + 1);
   return new Response(`${iCalendar}`, {
@@ -17,8 +20,16 @@ export async function handleRequest(request: Request): Promise<Response> {
 
 function getICalendar(url: string): Promise<string> {
   return fetch(url)
-    .then(response => response.text())
+    .then(response => {
+      if (response.headers.get('content-type') !== 'text/calendar') {
+        return '';
+      }
+      return response.text();
+    })
     .then(text => {
+      if (!text) {
+        return '';
+      }
       return text.replace(/(\r?\n?)END:VEVENT/g, '$1X-MICROSOFT-CDO-BUSYSTATUS:TENTATIVE$1END:VEVENT');
     });
 }
